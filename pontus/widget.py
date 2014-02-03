22d74e052dbc567401d32302660c7bd06ae65800
amen souissi <amensouissi@ecreall.com>
2014-02-03 15:46:02 +0100

    schema, multiple form and widget

--- pontus/widget.py
+++ pontus/widget.py
@@ -3,16 +3,16 @@ from deform.widget import SequenceWidget, MappingWidget
 
 class TableWidget(SequenceWidget):
   
-    template = 'sdkuneagi:templates/table.pt'
-    item_template = 'sdkuneagi:templates/table_item.pt'
-    readonly_template = 'sdkuneagi:templates/readonly/table.pt'
-    readonly_item_template = 'sdkuneagi:templates/readonly/table_item.pt'
+    template = 'pontus:templates/table.pt'
+    item_template = 'pontus:templates/table_item.pt'
+    readonly_template = 'pontus:templates/readonly/table.pt'
+    readonly_item_template = 'pontus:templates/readonly/table_item.pt'
 
 class LineWidget(MappingWidget):
   
-    template = 'sdkuneagi:templates/mapping.pt'
-    item_template = 'sdkuneagi:templates/mapping_item.pt'
-    readonly_template = 'sdkuneagi:templates/readonly/mapping.pt'
-    readonly_item_template = 'sdkuneagi:templates/readonly/mapping_item.pt'  
+    template = 'pontus:templates/mapping.pt'
+    item_template = 'pontus:templates/mapping_item.pt'
+    readonly_template = 'pontus:templates/readonly/mapping.pt'
+    readonly_item_template = 'pontus:templates/readonly/mapping_item.pt'  
 
 
