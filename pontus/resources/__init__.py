86549b371586dbd51c8f49a7d96228531c8e30b4
Vincent Fretin <vincent.fretin@gmail.com>
2014-03-24 11:27:49 +0100

    use unicode

--- pontus/resources/__init__.py
+++ pontus/resources/__init__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
-IndexViewErrorPrincipalmessage = 'Vous n\'avez pas les droits suffisants pour accéder à l\'information demandée.'
-IndexViewErrorCauses = ['Vous ne disposez pas des rôles nécessaires.',
-		        'Certaines conditions ne sont pas encore satisfaites.']
-IndexViewErrorSolutions = ['Contactez l\'administrateur...']
+IndexViewErrorPrincipalmessage = u"Vous n'avez pas les droits suffisants pour accéder à l'information demandée."
+IndexViewErrorCauses = [u"Vous ne disposez pas des rôles nécessaires.",
+                        u"Certaines conditions ne sont pas encore satisfaites."]
+IndexViewErrorSolutions = [u"Contactez l'administrateur..."]
 
-MutltipleViewErrorPrincipalmessage = 'Vous n\'avez pas les droits suffisants pour accéder à l\'ensemble des informations demandées.'
-MutltipleViewErrorCauses = ['Vous ne disposez pas des rôles nécessaires.',
-		            'Certaines conditions ne sont pas encore satisfaites.']
-MutltipleViewErrorSolutions = ['Contactez l\'administrateur...']
+MutltipleViewErrorPrincipalmessage = u"Vous n'avez pas les droits suffisants pour accéder à l'ensemble des informations demandées."
+MutltipleViewErrorCauses = [u"Vous ne disposez pas des rôles nécessaires.",
+                            u"Certaines conditions ne sont pas encore satisfaites."]
+MutltipleViewErrorSolutions = [u"Contactez l'administrateur..."]
 
-CallViewErrorPrincipalmessage = 'Vous n\'avez pas les droits suffisants pour appliquer l\'action suivante aux informations demandées.'
-CallViewViewErrorCauses =  ['Vous ne disposez pas des rôles nécessaires.',
-		            'Certaines conditions ne sont pas encore satisfaites.']
+CallViewErrorPrincipalmessage = u"Vous n'avez pas les droits suffisants pour appliquer l'action suivante aux informations demandées."
+CallViewViewErrorCauses =  [u"Vous ne disposez pas des rôles nécessaires.",
+                            u"Certaines conditions ne sont pas encore satisfaites."]
