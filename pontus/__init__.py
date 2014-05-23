e352996556a1c2fc2fbe455669bb1c8c5d376bf3
Cédric Messiant <cedricmessiant@ecreall.com>
2014-05-23 18:03:32 +0200

    Remove trailing lines

--- pontus/__init__.py
+++ pontus/__init__.py
@@ -3,5 +3,3 @@ def includeme(config): # pragma: no cover
     config.scan('.')
     YEAR = 86400 * 365
     config.add_static_view('pontusstatic', 'pontus.dace_ui_extension:static', cache_max_age=YEAR)
-
-
