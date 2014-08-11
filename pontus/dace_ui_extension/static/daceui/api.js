8224f7d31e749a0362a5d6673849db8cfa79fd7a
Vincent Fretin <vincent.fretin@gmail.com>
2014-08-11 19:41:25 +0200

    use $(target).offset().top instead of $(target)[0].getBoundingClientRect().top

--- pontus/dace_ui_extension/static/daceui/api.js
+++ pontus/dace_ui_extension/static/daceui/api.js
@@ -10,7 +10,7 @@ function update_action(url){
               var target = $(this).closest('.dace-action').data('target')+'-modal';
               if (Boolean(toreplay)){$(target).modal('show'); return false}
               var url = $(this).closest('.dace-action').data('updateurl');
-              $.getJSON(url,{tomerge:'True', coordinates:'main'}, function(data) { 
+              $.getJSON(url,{tomerge:'True', coordinates:'main'}, function(data) {
                  var action_body = data['body'];
                  if (action_body){
                      $($(target).find('.modal-body')).html(action_body);
@@ -31,9 +31,9 @@ $(function() {
 
 $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
     // Avoid following the href location when clicking
-    event.preventDefault(); 
+    event.preventDefault();
     // Avoid having the menu to close when clicking
-    event.stopPropagation(); 
+    event.stopPropagation();
     // If a menu is already open we close it
     //$('ul.dropdown-menu [data-toggle=dropdown]').parent().removeClass('open');
     // opening the one you clicked on
@@ -41,9 +41,9 @@ $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
 
     var menu = $(this).parent().find("ul");
     var menupos = menu.offset();
-  
+
     if ((menupos.left + menu.width()) + 30 > $(window).width()) {
-        var newpos = - menu.width();      
+        var newpos = - menu.width();
     } else {
         var newpos = $(this).parent().width();
     }
@@ -56,7 +56,7 @@ $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
           var url = $(this).attr('href');
           $.get(url, {}, function(data) {
                 $(target).html($(data).find(target).html());
-                $("html, body").animate({ scrollTop: $(target)[0].getBoundingClientRect().top }, "slow");
+                $("html, body").animate({ scrollTop: $(target).offset().top }, "slow");
           });
           return false;
     });
