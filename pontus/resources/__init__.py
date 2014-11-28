3ece0aff70b89bfdeb6ff8b114c09455c3258ec9
amen souissi <amensouissi@ecreall.com>
2014-11-28 11:35:57 +0100

    Add licence

--- pontus/resources/__init__.py
+++ pontus/resources/__init__.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+# Copyright (c) 2014 by Ecreall under licence AGPL terms 
+# avalaible on http://www.gnu.org/licenses/agpl.html 
+
+# licence: AGPL
+# author: Amen Souissi
+
 
 IndexViewErrorPrincipalmessage = u"Vous n'avez pas les droits suffisants pour accéder à l'information demandée."
 IndexViewErrorCauses = [u"Vous ne disposez pas des rôles nécessaires.",
