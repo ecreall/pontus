65a6750f10754b0cb7fb26566d644a157508c2f8
amen souissi <amensouissi@ecreall.com>
2015-03-27 17:54:54 +0100

    fix file widget

--- pontus/static/js/file_upload.js
+++ pontus/static/js/file_upload.js
@@ -1,7 +1,6 @@
 
 function init_input(oid){
-
-    $("#"+oid).fileinput({'showUpload': false, 
+    $("#"+oid).fileinput({'showUpload': false,
     	                  'previewFileType':'any',
     	                  'previewClass': 'pontus-file-preview',
     	                  'previewSettings': {image: {width: "auto", height: "90px"},
