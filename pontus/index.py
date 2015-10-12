79f60f0152ac5261cc6078a4ce11f619b8edc09a
Amen Souissi <amensouissi@ecreall.com>
2015-10-12 11:14:54 +0200

    fix sequence widget

--- pontus/index.py
+++ pontus/index.py
@@ -15,6 +15,7 @@ from pontus.resources import (
     IndexViewErrorPrincipalmessage, IndexViewErrorCauses)
 from pontus import _
 
+
 @view_config(
     name='index',
     context=IObject,
@@ -25,12 +26,12 @@ class Index(View):
     title = _('Index')
     viewid = 'index'
 
-    def __init__(self, 
-                 context, 
-                 request, 
-                 parent=None, 
-                 wizard=None, 
-                 index=0, 
+    def __init__(self,
+                 context,
+                 request,
+                 parent=None,
+                 wizard=None,
+                 index=0,
                  **kwargs):
         View.__init__(self, context, request, parent, wizard, index, **kwargs)
         self.title = self.context.title
@@ -44,7 +45,7 @@ class Index(View):
 
         if views:
             indexmultiview = MultipleView(
-                 self.context, self.request, self.parent)
+                self.context, self.request, self.parent)
             indexmultiview.coordinates = self.coordinates
             indexmultiview._init_views(views)
             indexmultiview.before_update()
