61a05e0743637f916d373d0fec31406aa7684914
Vincent Fretin <vincent.fretin@gmail.com>
2016-09-15 15:19:58 +0200

    Back to development: 1.0.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
     ]
 
 setup(name='ecreall_pontus',
-      version='1.0.2',
+      version='1.0.3.dev0',
       description='An application programming interface built upon the Pyramid web framework and substanced application. It provides libraries which make it easy to manage complex and imbricated views. For that purpose, Pontus introduces the concept of operations on views.',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
